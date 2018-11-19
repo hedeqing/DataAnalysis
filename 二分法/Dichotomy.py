3b39022300e16d9414785fd1393c395dea894701
240484406@qq.com <240484406@qq.com>
2018-11-19 21:01:06 +0800

    试位法与二分法

---
+++ 二分法/Dichotomy.py
@@ -0,0 +1,61 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 18 16:37:31 2018
+
+@author:何德庆
+"""
+
+import matplotlib.pyplot as plt
+
+a_x = []
+a_abs = []
+a_y = []
+e = 5e-5
+def fun(x):
+    """
+    函数初始化
+    param x: 输入
+    """
+    return 2*x**3+3*x**2-6
+def show_fun():
+    """
+    可视化函数
+    """
+    b_x= []
+    b_y = []
+    for i in range(-50,50):
+        b_x.append(i)
+        b_y.append(fun(i))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot(b_x,b_y)
+    plt.show()
+    
+def calculate(a,b):
+    """
+    逼近函数
+    param a :下限值
+    param b :上限值
+    返回值  x： 近似根
+    """
+    epoch = 0
+    x = (a + b)/2.0
+    while True:
+        if abs((b-a)/b) < e:
+            return x
+        if  fun(a)*fun(x)<0:
+            b = x
+        else:
+            a = x
+        epoch=epoch+1
+        a_x.append(epoch)
+        a_abs.append(abs((b-a)/b))
+        x = (a+b)/2.0
+if __name__ =="__main__":    
+    show_fun()
+    print(calculate(-5, 5))
+    plt.figure(figsize=(10,5))
+    plt.xlabel("epoches")
+    plt.ylabel('abs')
+    plt.plot(a_x, a_abs,label='abs',color='red')
+    plt.show()
---
+++ 试位法/TestPositionMethod.py
@@ -0,0 +1,61 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 18 18:43:02 2018
+
+@author: 24048
+"""
+
+
+import matplotlib.pyplot as plt
+
+e = 5e-5
+a_x = []
+a_abs = []
+def fun(x):
+    """
+    函数初始化
+    param x: 输入
+    """
+    return x**2+8*x-6
+
+def show_fun():
+    """
+    可视化函数
+    """
+    b_x= []
+    b_y = []
+    for i in range(-50,50):
+        b_x.append(i)
+        b_y.append(fun(i))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.plot(b_x,b_y)
+    plt.show()
+def calculate(a,b):
+    """
+    逼近函数
+    param a :下限值
+    param b :上限值
+    返回值  x： 近似根
+    """
+    epoch = 0
+    x=b-fun(b)*(a-b)/(fun(a)-fun(b))
+    while True:
+        if abs((b-a)/b) < e:
+            return x
+        if  fun(a)*fun(x)<0:
+            b = x
+        else:
+            a = x
+        a_x.append(epoch)
+        a_abs.append(abs((b-a)/b))
+        epoch = epoch+1
+        x=b-fun(b)*(a-b)/(fun(a)-fun(b))         
+if __name__=="__main__":
+    show_fun()
+    print(calculate(-5,5))
+    plt.xlabel('epoches')
+    plt.ylabel('abs')
+    plt.plot(a_x,a_abs)
+    plt.show()
+    
